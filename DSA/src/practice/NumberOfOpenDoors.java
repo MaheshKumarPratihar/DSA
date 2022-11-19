7ba5eb25a9038a4a4a7ef580ad04a32393c08f79
MaheshKumarPratihar <sam43592@gmail.com>
2022-11-19 15:44:10 +0530

    NoOfOpenGates

--- DSA/src/practice/NumberOfOpenDoors.java
+++ DSA/src/practice/NumberOfOpenDoors.java
@@ -2,26 +2,42 @@ package practice;
 
 import java.util.Arrays;
 
+//Idea behind:
+//
+//This is so because each gate is visited by its factors only.
+//For example 100 will be visited by 1,2,4,5,10,20,25,50,100 in that order.  
+//Only perfect squares have an odd number of factors (try proving this). 
+//Hence the answer for N will be the number of perfect squares less than or equal to N.
+
 public class NumberOfOpenDoors {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int noOfOpenDoors = noOfOpenDoors((long) 4);
+
+		System.out.println("Number of opened doors are --> " + noOfOpenDoors);
 
 	}
-	
-	
+
 	public static int noOfOpenDoors(Long N) {
-        Long totalOpenedDoors = N;
-        
-        int[] array = new int[10];
-        
-        Arrays.fill(array, 0);
-        
-        for(int i = 2; i <= N; i++){
-            
-        }
-        
-        return 0;
-    }
+
+		if (N == 2 || N == 1 || N == 3) {
+			return 1;
+		}
+
+		int countSquares = 1;
+
+		for (int i = 4; i <= N; i++) {
+
+			if (i % 2 == 0) {
+				double sqrt = Math.sqrt(i);
+				if (sqrt - Math.floor(sqrt) == 0) {
+					countSquares++;
+				}
+			}
+
+		}
+
+		return countSquares;
+	}
 
 }
